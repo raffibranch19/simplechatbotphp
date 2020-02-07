a410b9c6a76f5b3417b4ecbba2a08e16329183e0
Mohamad Raffi Abdul Azis <raffiazisgit19@gmail.com>
2020-02-07 21:48:20 +0700

    First Commit

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+ 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+ 
+use \LINE\LINEBot;
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+
+$pass_signature = true;
+
+// set LINE channel_access_token and channel_secret
+$channel_access_token = "";
+$channel_secret = "";
+
+// inisiasi objek bot
+$httpClient = new CurlHTTPClient($channel_access_token);
+$bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+ 
+$app = AppFactory::create();
+$app->setBasePath("/public");
+ 
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello World!");
+    return $response;
+});
